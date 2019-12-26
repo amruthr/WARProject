71497df64d539af91d10049aa938ce0d819b5ad1
amruthr <34576243+amruthr@users.noreply.github.com>
2019-12-26 18:55:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 module.exports = {
   secret: 'MY SECRET',
   DB_URI_DEV: 'mongodb://localhost:27017/admin',
-  DB_URI: 'mongodb+srv://amruthr:<password>@cluster0-lyrkr.mongodb.net/test?retryWrites=true&w=majority'
+  DB_URI: process.env.DB_URI,
 };
